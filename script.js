23bfd0ea083e035d18317607f25b066b00e6ede2
sirin-acciojob-26 <166235930+sirin-acciojob-26@users.noreply.github.com>
2024-07-21 22:30:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 function firstWord(s) {
   // your code here
+	  // Trim any leading or trailing whitespace from the input string
+    str = str.trim();
+	
+    
+    // Find the index of the first space
+    const spaceIndex = str.indexOf(' ');
+    
+    // If there is no space, return the entire string
+    if (spaceIndex === -1) {
+        return str;
+    }
+
+	
+    // Return the substring from the start up to the first space
+    return str.substring(0, spaceIndex);
 }
 
-// Do not change the code below
+// // Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
